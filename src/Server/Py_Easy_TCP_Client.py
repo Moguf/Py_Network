13e351e9955464a5d65bd3dee3642438cfe9ed92
Mogu programming <kbu94984@gmail.com>
2016-08-27 21:45:29 +0900

    MOD: Py_easy_tcp_....py

--- src/Server/Py_Easy_TCP_Client.py
+++ src/Server/Py_Easy_TCP_Client.py
@@ -2,13 +2,12 @@
 
 import socket
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 Server_ip = '127.0.0.1'
 port = 12345
-s.connect((Server_ip,port))
+s.connect((Server_ip, port))
 print(s.recv(1024))
 text = "Hello Server"
 data = text.encode('ascii')
 s.send(data)
 s.close()
-
